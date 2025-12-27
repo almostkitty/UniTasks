2ea85c238e2fd789cab8c5d30538898c6c98334a
Herman <palchukgerman@gmail.com>
2025-12-27 19:19:14 +0300

    New LW 'OAuth' Added

---
+++ sem7/prog7/LW_OAuth2/oauth_client.py
@@ -0,0 +1,29 @@
+from requests_oauthlib import OAuth2Session
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+REDIRECT_URI = "http://localhost:8000/callback"
+AUTHORIZATION_BASE_URL = "https://github.com/login/oauth/authorize"
+TOKEN_URL = "https://github.com/login/oauth/access_token"
+SCOPE = ["read:user"] # выберите необходимые scopes
+
+oauth = OAuth2Session(CLIENT_ID, redirect_uri=REDIRECT_URI, scope=SCOPE)
+authorization_url, state = oauth.authorization_url(AUTHORIZATION_BASE_URL)
+print("Перейдите по ссылке для авторизации:", authorization_url)
+
+redirect_response = input("Вставьте полный URL перенаправления: ")
+token = oauth.fetch_token(
+    TOKEN_URL,
+    authorization_response=redirect_response,
+    client_secret=CLIENT_SECRET
+    )
+print("Токен:", token)
+
+r = oauth.get("https://api.github.com/user")
+print("Ваши данные:", r.json())
